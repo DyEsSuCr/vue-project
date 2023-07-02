c926ea78ad974d0bf284cd418babb7fa729bc2be
DylanSuarez <2000sajo@gmail.com>
2023-07-02 13:54:13 -0500

    FormLogin

---
+++ src/components/auth/FormLogin.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+import { signin } from '../../services/auth/signin'
+
+const dataForm = ref({
+  email: '',
+  password: ''
+})
+
+const data = () => {
+  signin({ ...dataForm.value })
+}
+</script>
+
+<template>
+  <form @submit.prevent="data">
+    <h1>Inicia sesión</h1>
+    <input type="email" placeholder="correo" v-model="dataForm.email">
+    <input type="text" placeholder="contraseña" v-model="dataForm.password">
+    <button>Ingresar</button>
+  </form>
+</template>
